f06d886951485a9234e4b9555f23d84bf01ce1e2
Jordan Wine <jordandwine@gmail.com>
2020-11-02 06:38:28 -0800

    fixing CORS

--- src/main/java/com/drchunks/brokerchunks/api/ApiApplication.java
+++ src/main/java/com/drchunks/brokerchunks/api/ApiApplication.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -19,6 +22,18 @@ public class ApiApplication {
 		for (String beanName : beanNames) {
 			System.out.println(beanName);
 		}
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			/*
+			 * @Override public void addCorsMappings(CorsRegistry registry) { System.out.
+			 * println("=============================adding CORS======================================="
+			 * ); registry.addMapping("greetings").allowedOrigins("http://localhost:8081");
+			 * }
+			 */
+		};
 	}
 
 }
--- src/main/java/com/drchunks/brokerchunks/api/GreetingRepository.java
+++ src/main/java/com/drchunks/brokerchunks/api/GreetingRepository.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "greeting", path = "greeting")
+@RepositoryRestResource(collectionResourceRel = "greetings", path = "greetings")
+@CrossOrigin(origins = "http://localhost:8081")
 public interface GreetingRepository extends MongoRepository<Greeting, String> {
 
     List<Greeting> findByContent(@Param("content") String content);
